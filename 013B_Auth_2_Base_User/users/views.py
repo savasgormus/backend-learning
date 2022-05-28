c2fcaf751c34a3949a3899be51ab359f5951a404
savasgormus <savasgormus1@gmail.com>
2022-05-28 12:00:48 +0300

    update

--- 013B_Auth_2_Base_User/users/views.py
+++ 013B_Auth_2_Base_User/users/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, redirect, HttpResponse
 
 from django.contrib import messages
+from django.contrib.auth import logout, login, authenticate
 
 # Create your views here.
 
 def home(request):
     return render(request, 'users/home.html')
+
+def user_logout(request):
+    messages.success(request, 'You logged out!')
+    logout(request)
+    return redirect('home')
