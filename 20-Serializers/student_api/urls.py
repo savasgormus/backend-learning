d0960280d57d4ae7cdcf0169ab158f054c06f806
Savaş Görmüş <savasgormus1@gmail.com>
2022-06-11 12:12:02 +0300

    update

---
+++ 20-Serializers/student_api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import home, student_api, student_api_get_update_delete
+
+urlpatterns = [
+    path('', home),
+    path('students/', student_api),
+    path('students/<int:pk>', student_api_get_update_delete),
+]
