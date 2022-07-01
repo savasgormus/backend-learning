390e7b1c38fc254128e92ac581af63c3ca774653
Savaş Görmüş <savasgormus1@gmail.com>
2022-07-01 16:38:59 +0300

    update

--- 06-django-crud-operations/fscohort/models.py
+++ 06-django-crud-operations/fscohort/models.py
@@ -3,13 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
-    first_name : models.CharField(max_length=30)
-    last_name : models.CharField(max_length=30)
-    number : models.IntegerField(blank=True, null=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    number = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
-    class Meta:
-        #admin paneldeki ismini Öğrenciler olarak değiştirdik.
-        verbose_name_plural = 'Öğrenciler'
+        return f"{self.first_name} {self.last_name}"
         
