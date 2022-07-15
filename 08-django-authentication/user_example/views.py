a1e2c7afab63ba52b06017aab7952a43e3cee5a2
Savaş Görmüş <savasgormus1@gmail.com>
2022-07-15 19:23:52 +0300

    update

--- 08-django-authentication/user_example/views.py
+++ 08-django-authentication/user_example/views.py
@@ -10,28 +10,30 @@ def special(request):
     return render(request, "user_example/special.html")
 
 def register(request):
+    
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-        # post ile veri geldi. eğer form valid ise kaydet
-        # fakat authenticate için yeterli değil
-        # aşağıda bu işlemi tamamlayacağız.
+            # post ile veri geldi. eğer form valid ise kaydet
+            # fakat authenticate için yeterli değil
+            # aşağıda bu işlemi tamamlayacağız.
 
             # username ve password'u sayfadan çekiyoruz.
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
-
+            
             # yukarıdan çektiğimiz kullanıcı adı ve şifreyle user'ı oluşturduk ve authenticate işlemine tabi tuttuk.
             user = authenticate(username=username, password=password)
-
+            
             # kullanıcının login olmasını istiyoruz ve bunun için de user gerekli.
-            login(request,user)
+            login(request, user)
 
             # login olduktan sonra anasayfaya redirect ediyoruz.
             return redirect('home')
-    form = UserCreationForm()
+            
+    form = UserCreationForm()  
     context = {
-        'form' : form
+        'form':form
     }
     return render(request, "registration/register.html", context)
